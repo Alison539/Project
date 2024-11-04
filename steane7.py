6df2b58eb1b25f262d8f33f55efbd759b607b321
Alison <ad2160@cam.ac.uk>
2024-11-04 14:55:57 +0000

    Tested Steane 7

--- steane7.py
+++ steane7.py
@@ -1,4 +1,6 @@
-import stim
+import stim, numpy as np
+
+#Steane 7 qubit code (aka [7,1,3])
 
 pregen = stim.Circuit.generated(
     "repetition_code:memory",
@@ -10,10 +12,14 @@ pregen = stim.Circuit.generated(
     before_round_data_depolarization=0.04,
     #means an x-error occurs with this probability each measurement operation
     before_measure_flip_probability=0.01)
-print(repr(pregen))
+#print(repr(pregen))
 
 #decided to make for same model of error as above
 
+#0-5 = the 6 generators
+
+#Initial working = made using the picture of the error correcting circuit from Mermin's Chap 5
+#however realised that basically have now made the generators, but don't understand what to do with the generators
 circuit = stim.Circuit("""
     R 0 1 2 3 4 5 6 7 8 9 10 11 12    
     H 0 1 2 3 4 5
@@ -60,15 +66,77 @@ circuit = stim.Circuit("""
         TICK
         M 0 1 2 3 4 5
 
-            DETECTOR rec[-1] rec[-7] 
-            DETECTOR rec[-2] rec[-8] 
-            DETECTOR rec[-3] rec[-9] 
-            DETECTOR rec[-4] rec[-10] 
-            DETECTOR rec[-5] rec[-11] 
-            DETECTOR rec[-6] rec[-12]     
+        DETECTOR rec[-1] rec[-7] 
+        DETECTOR rec[-2] rec[-8] 
+        DETECTOR rec[-3] rec[-9] 
+        DETECTOR rec[-4] rec[-10] 
+        DETECTOR rec[-5] rec[-11] 
+        DETECTOR rec[-6] rec[-12]     
         }           
 """)
 
 #adding a sampler
-sampler = circuit.compile_detector_sampler()
-print(sampler.sample(shots=5))
+
+
+#using steane 7 that found online
+#Project into Steane's seven qubit code, up to sign:
+#These are the qubits used to represent the logical qubit
+# 0123456
+# ___ZZZZ
+# _ZZ__ZZ
+# Z_Z_Z_Z
+# ___XXXX
+# _XX__XX
+# X_X_X_X
+
+#The MPP gates, give you +1 or -1 eigenvalues of those (which can be used to figure out where the error ocurred)
+
+#basically did the generators
+#v.impo detector just gets the result of the last measurement that occurred
+original_circuit_1 = ("""
+
+MPP Z3*Z4*Z5*Z6
+MPP Z1*Z2*Z5*Z6
+MPP Z0*Z2*Z4*Z6
+MPP X3*X4*X5*X6
+MPP X1*X2*X5*X6
+MPP X0*X2*X4*X6
+""")
+
+original_circuit_2 = ("""
+REPEAT 1{
+    MPP Z3*Z4*Z5*Z6
+    MPP Z1*Z2*Z5*Z6
+    MPP Z0*Z2*Z4*Z6
+    MPP X3*X4*X5*X6
+    MPP X1*X2*X5*X6
+    MPP X0*X2*X4*X6
+
+    DETECTOR rec[-6] rec[-12] 
+    DETECTOR rec[-5] rec[-11] 
+    DETECTOR rec[-4] rec[-10] 
+    DETECTOR rec[-3] rec[-9] 
+    DETECTOR rec[-2] rec[-8] 
+    DETECTOR rec[-1] rec[-7] 
+                           
+}
+                          """)
+
+noErrors = stim.Circuit(original_circuit_1 + original_circuit_2)
+# Z stabilizers were already satisfied by starting in |0000000>
+sampler = noErrors.compile_detector_sampler()
+#This returns all false (which means that eigenvalues all +1 therefore identity no changes)
+print(sampler.sample(shots=1))
+print(np.sum(sampler.sample(shots=1)))
+
+#the purpose of steane 7 is that can detect (and the correct afterwards) any singular XYZ errors that occur on the data qubits
+#testing my steane code
+xError = stim.Circuit(original_circuit_1 + "X_ERROR(1) 4 \n" + original_circuit_2)
+sampler = xError.compile_detector_sampler()
+#Sampler should return [ True False  True False False False] //the True is where Z4 appears in the generator (since X4 anticommutes with Z4) and it does
+print(sampler.sample(shots=1))
+
+yError = stim.Circuit(original_circuit_1 + "Y_ERROR(1) 2 \n" + original_circuit_2)
+sampler = yError.compile_detector_sampler()
+#Sampler should return [False True True False True True]
+print(sampler.sample(shots=1))
